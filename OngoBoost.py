afbeb7adc018e6643791b833fefad4d4a175af59
Sayantan Mitra <37726559+SayantanMitra87@users.noreply.github.com>
2020-02-10 15:48:41 -0500

    Update OngoBoost.py

--- OngoBoost.py
+++ OngoBoost.py
@@ -131,7 +131,7 @@ else:
         st.write("")
         
         
-st.markdown("""<iframe src="https://docs.google.com/presentation/d/e/2PACX-1vRZfuQaQSz6L5F23l_E6SmhuNUJLGYOUdL4QYtwPplWfnhijze0ZteVXZkx8jWhD2BxJGn6WznXY_co/embed?start=false&loop=false&delayms=3000" frameborder="0" width="480" height="329" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true"></iframe>""",unsafe_allow_html=True)
+st.markdown("""https://docs.google.com/presentation/d/e/2PACX-1vRZfuQaQSz6L5F23l_E6SmhuNUJLGYOUdL4QYtwPplWfnhijze0ZteVXZkx8jWhD2BxJGn6WznXY_co/pub?start=false&loop=false&delayms=3000""",unsafe_allow_html=True)
 
 
 #else:
